b00d37cc697cde839e85ef8a089da90b76981649
Steven Burnett <sburnett@instructure.com>
2019-05-17 15:35:55 -0600

    make api request to kaltura including config
    refs CORE-2661
    
    Test Plan:
    - N/A
    
    Change-Id: I10b52b793e0d70403ddd6983c2b6ec5a815b9e73
    Reviewed-on: https://gerrit.instructure.com/194237
    Tested-by: Jenkins
    Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
    QA-Review: Clay Diffrient <cdiffrient@instructure.com>
    Product-Review: Clay Diffrient <cdiffrient@instructure.com>

--- app/api/kaltura.js
+++ app/api/kaltura.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function canvasPath() {
-  return "/api/v1/services/kaltura_session";
+  return "/api/v1/services/kaltura_session?include_upload_config=1";
 }
 
 module.exports = { canvasPath };
